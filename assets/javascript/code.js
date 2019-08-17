f02ecdb8dd67ddb0e87f49219bda04eba41e370d
Noah Sylwester <nanoabram@comcast.net>
2019-08-17 01:45:58 -0700

    Adjusted css, began initial work on a favorites functionality. Intending to allow users to select gifs as favorites, which will add them to a favorites array that the favorites menu will fill itself with.

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -28,7 +28,7 @@ var newTopic = "";
     }).then((response) => {
       console.log(response);
       response.data.forEach((gif) => {
-        arr.push($(`<div class="gif"><p class="gif-rating">Rating: ${gif.rating}</p><img class="gif-image" alt="gif" src="${gif.images.downsized_still.url}" data-alt="${gif.images.downsized.url}"></div>`))
+        arr.push($(`<div class="gif"><p class="gif-rating">Title: ${gif.title}<br />Rating: ${gif.rating}<br /><a href="${gif.source_post_url}">Source</a></p><img class="gif-image" alt="gif" src="${gif.images.downsized_still.url}" data-alt="${gif.images.downsized.url}"></div>`))
       });
       gifDisplay.empty().append(...arr);
     });
